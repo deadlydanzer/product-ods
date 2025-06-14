10de87886ba4339d4e8be11f17b934fd66e97f34
deadlydanzer <padyalabhijit@fulikzo.com>
2025-06-14 23:58:46 +0530

    created script for product detail

---
+++ src/db/V0.0.1__create_product_detail_table.sql
@@ -0,0 +1,20 @@
+-- PRODUCT DETAIL TABLE
+
+DROP TABLE IF EXISTS PRODUCT_DETAIL
+;
+
+CREATE TABLE PRODUCT_DETAIL (
+     PD_ID            BIGSERIAL                     NOT NULL
+    ,PD_NAME          VARCHAR                       NOT NULL
+    ,PD_TYPE          VARCHAR                       NOT NULL
+    ,PD_CATEGORY      VARCHAR                       NULL
+    ,PD_DESC          TEXT                          NULL
+    ,PD_PRICE         BIGDECIMAL(10,2)              NULL
+    ,PD_SOURCE_ID     BIGINT                        NULL
+    ,PD_CREATED_BY    VARCHAR                       NOT NULL
+    ,PD_CREATED_TS    TIMESTAMP WITHOUT TIME ZONE   NOT NULL    DEFAULT NOW()
+    ,PD_MODIFIED_TS   TIMESTAMP WITHOUT TIME ZONE   NULL
+    ,PD_STATUS        VARCHAR                       NOT NULL    DEFAULT 'ACTIVE'
+    ,PRIMARY KEY (PD_ID)
+    ,UNIQUE (PD_ID, PD_SOURCE_ID)
+);
