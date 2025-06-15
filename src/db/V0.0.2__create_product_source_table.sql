3dbf6a07aae2e3abe1ce5e058b46ab1afa47cc24
SwaraliDoshi97 <swaralidoshi222@gmail.com>
2025-06-15 23:06:05 +0530

    fix user email

---
+++ src/db/V0.0.2__create_product_source_table.sql
@@ -0,0 +1,22 @@
+-- PRODUCT_SOURCE_TABLE
+
+DROP TABLE IF EXISTS PRODUCT_SOURCE
+;
+
+CREATE TABLE PRODUCT_SOURCE(
+    PS_ID           BIGSERIAL                   NOT NULL,
+    PD_ID           BIGSERIAL                   NOT NULL,
+    PS_1            VARCHAR                     NULL,
+    PS_1_LINK       VARCHAR                     NULL,
+    PS_2            VARCHAR                     NULL,
+    PS_2_LINK       VARCHAR                     NULL,
+    PS_3            VARCHAR                     NULL,
+    PS_3_LINK       VARCHAR                     NULL,
+    PS_CREATED_BY   VARCHAR                     NOT NULL,
+    PS_CREATED_TS  TIMESTAMP WITHOUT TIME ZONE  NOT NULL    DEFAULT NOW(),
+    PS_MODIFED_TS  TIMESTAMP WITHOUT TIME ZONE  NULL,
+    PRIMARY KEY(PS_ID)
+    UNIQUE(PD_ID)
+    FOREIGN KEY(PD_ID) REFERENCES PRODUCT_DETAIL(PD_ID)
+     
+);
